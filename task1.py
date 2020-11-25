0536f21a4f563ed3042ebeb0f0ccda756bb0562b
OlyaBezrukova <74977261+OlyaBezrukova@users.noreply.github.com>
2020-11-25 14:21:22 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+a, b = int(input()), int(input())
+if a < b:
+    for i in range(a, b+1):
+        print(i, end=' ')
+elif a > b:
+    for i in range(a, b-1, -1):
+        print(i, end=' ')
+else:
+    print(a)
+    
